1a8698f2af76e4a504022bd68d39e087603f4e13
Kristian Epps <kristian@xepps.com>
2017-10-06 01:04:57 +0100

    jest working

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     modulePathIgnorePatterns: [`/.node_modules.+/`],
-    testPathIgnorePatterns: [`/node_modules/`, `/.node_modules.+/`]
+    testPathIgnorePatterns: [`/node_modules/`, `/.node_modules.+/, /*.css/`],
+    setupFiles: ["<rootDir>/test/setup/shim.js", "<rootDir>/test/setup/enzyme-adapter.js"],
+    moduleNameMapper: {
+        "\\.(css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js"
+    }
 };
